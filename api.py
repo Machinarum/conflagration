680bcc69479fceb9064815c6a2e9dfba7d01b3f4
Daryl Walleck <daryl.walleck@rackspace.com>
2017-04-06 15:18:12 -0500

    Fixed jidar's typo

--- api.py
+++ api.py
@@ -4,14 +4,16 @@ from collections import namedtuple
 class Conflagration(object):
     def __init__(
             self, files=None, dirs=None, recursive=False, include_env=True):
-        fir_files = self._parse_dirs(*dirs, recurive=recursive)
+        dir_files = self._parse_dirs(*dirs, recurive=recursive)
         _config_wrappers = [wrap.config_file(f) for f in files]
         _env_wrapper = wrap.environment()
 
     def _parse_dirs(*dirs, recursive=False):
-        files = 
+        files = []
+        for d in dirs:
+            for dirpath, directories, filenames in os.walk(d):
+                files.extend([os.path.join(dirpath, f) for f in filenames])
 
-        pass
         # smash all these data sources together and
         # return a Conflagration that's been initialized with that
         # file list.
