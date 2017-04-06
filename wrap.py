9e8ec05d6dc8bbe3acef9e4e971d3ccc0b761c06
Daryl Walleck <daryl.walleck@rackspace.com>
2017-04-06 14:39:10 -0500

    Added env variable loader

--- wrap.py
+++ wrap.py
@@ -1,5 +1,8 @@
 from ConfigParser import SafeConfigParser
 from collections import namedtuple
+from future.utils import isidentifier
+import logging
+import os
 import prettyprint
 
 
@@ -13,3 +16,20 @@ def config_file(file, name=None):
         sect_nts[s]=nt
     return namedtuple(
         name or 'config', sect_nts.keys())(**sect_nts)
+
+
+def environment(name='config', prefix='ENV_'):
+    env_vars = os.environ
+    filtered_vars = {
+        k: v for k, v in env_vars.iteritems() if k.startswith(prefix)
+    }
+    for k, v in filtered_vars.iteritems():
+        if not isidentifier(k):
+            logging.log(
+                logging.ERROR,
+                '{0} is not a valid proprty name.'.format(k))
+            raise Exception('{0} is not a valid proprty name.'.format(k))
+        else:
+            new_key = k.split(prefix)[1]
+            filtered_vars[new_key] = filtered_vars.pop(k)
+    return namedtuple(name, filtered_vars.keys())(**filtered_vars)
