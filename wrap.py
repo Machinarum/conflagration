67d0b83fbc74fddd04a0dcd91c08969396b00258
Jose Idar <jose.idar@gmail.com>
2017-04-06 22:45:34 -0500

    working dotted path section and env key names

--- wrap.py
+++ wrap.py
@@ -1,35 +1,45 @@
 from ConfigParser import SafeConfigParser
-from collections import namedtuple
-from future.utils import isidentifier
 import logging
 import os
-import prettyprint
 
 
-def config_file(file, name=None):
-    _data = SafeConfigParser()
-    _data.read(file)
-    sect_nts = {}
-    for s in _data.sections():
-        s_dict = {k: v for (k, v) in _data.items(s)}
-        nt = namedtuple(s, s_dict.keys())(**s_dict)
-        sect_nts[s]=nt
-    return namedtuple(
-        name or 'config', sect_nts.keys())(**sect_nts)
+def config_file(file):
+    data = SafeConfigParser()
+    data.read(file)
+    section_dict = {}
+    for s in data.sections():
+        for k, v in data.items(s):
+            section_dict['{}.{}'.format(s,k)]=v
+    return section_dict
 
-
-def environment(name='config', prefix='ENV_'):
+def environment(prefix='env.'):
     env_vars = os.environ
     filtered_vars = {
         k: v for k, v in env_vars.iteritems() if k.startswith(prefix)
     }
+    fdict={}
     for k, v in filtered_vars.iteritems():
-        if not isidentifier(k):
-            logging.log(
-                logging.ERROR,
-                '{0} is not a valid proprty name.'.format(k))
-            raise Exception('{0} is not a valid proprty name.'.format(k))
-        else:
-            new_key = k.split(prefix)[1]
-            filtered_vars[new_key] = filtered_vars.pop(k)
-    return namedtuple(name, filtered_vars.keys())(**filtered_vars)
+        clean_key = k.split(prefix)
+        new_key = None
+        if len(clean_key) > 1:
+            new_key = clean_key[1]
+        elif len(clean_key) == 1:
+            new_key = clean_key[0]
+        fdict[new_key] = filtered_vars[k]
+    return fdict
+
+# def environment(name='config', prefix='env.'):
+#     env_vars = os.environ
+#     filtered_vars = {
+#         k: v for k, v in env_vars.iteritems() if k.startswith(prefix)
+#     }
+#     for k, v in filtered_vars.iteritems():
+#         if not isidentifier(k):
+#             logging.log(
+#                 logging.ERROR,
+#                 '{0} is not a valid proprty name.'.format(k))
+#             raise Exception('{0} is not a valid proprty name.'.format(k))
+#         else:
+#             new_key = k.split(prefix)[1]
+#             filtered_vars[new_key] = filtered_vars.pop(k)
+#     return namedtuple(name, filtered_vars.keys())(**filtered_vars)
