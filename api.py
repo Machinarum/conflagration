7a393277795cd2be7bc0bbab14347becefb41efa
Jose Idar <jose.idar@rackspace.com>
2017-04-06 22:45:34 -0500

    working dotted path section and env key names

--- api.py
+++ api.py
@@ -1,19 +1,46 @@
-from conflagration import wrap
+import wrap
 from collections import namedtuple
 
-class Conflagration(object):
-    def __init__(
-            self, files=None, dirs=None, recursive=False, include_env=True):
-        dir_files = self._parse_dirs(*dirs, recurive=recursive)
-        _config_wrappers = [wrap.config_file(f) for f in files]
-        _env_wrapper = wrap.environment()
+def conflagration(files=None, dirs=None, recursive=False):
+    dirs = dirs or list()
+    dir_files = _parse_dirs(dirs, recursive=recursive)
+    files.extend(dir_files)
+    _superdict = dict()
+    for d in [wrap.config_file(f) for f in files]:
+        _superdict.update(d)
+    return _build_super_namedtuple(_superdict, 'conflagration')
 
-    def _parse_dirs(*dirs, recursive=False):
-        files = []
-        for d in dirs:
-            for dirpath, directories, filenames in os.walk(d):
-                files.extend([os.path.join(dirpath, f) for f in filenames])
+def _dotstring_to_nested_dict(dic, key, value):
+    k = key[0]
+    if len(key) > 1:
+        klist = key[1::]
+        if k in dic:
+            _dotstring_to_nested_dict(dic[k], klist, value)
+        else:
+            dic[k] = _dotstring_to_nested_dict(dict(), klist, value)
+    elif len(key) == 1:
+        dic[k] = value
+    return dic
 
-        # smash all these data sources together and
-        # return a Conflagration that's been initialized with that
-        # file list.
+def _dict_to_nt(d, name):
+    keys = d.keys()
+    finald = dict()
+    for k in keys:
+        if isinstance(d[k], dict):
+            finald[k] = _dict_to_nt(d[k], k)
+        else:
+            finald[k] = d[k]
+    return namedtuple(name, finald.keys(), verbose=True)(**finald)
+
+def _build_super_namedtuple(superdict, name):
+    nested_superdict = dict()
+    for k, v in superdict.items():
+        _dotstring_to_nested_dict(nested_superdict, k.split('.'), v)
+    return _dict_to_nt(nested_superdict, name)
+
+def _parse_dirs(dirs, recursive=False):
+    files = []
+    for d in dirs:
+        for dirpath, directories, filenames in os.walk(d):
+            files.extend([os.path.join(dirpath, f) for f in filenames])
+    return files
