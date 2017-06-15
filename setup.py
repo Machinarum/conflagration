003ec3d058532c610b2c5380018dd05974ff1f1e
Jose Idar <jose.idar@rackspace.com>
2017-06-15 16:35:22 -0500

    bumped version to 0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 # Normal setup stuff
 setup(
-    version='0.0.1'
+    version='0.0.1',
     name='conflagration',
     packages=find_packages(),
     zip_safe=False,
