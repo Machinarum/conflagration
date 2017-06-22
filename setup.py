d9162f9fe46b7fde6d571770d0b7ff545c65a21d
Jose Idar <jose.idar@rackspace.com>
2017-06-22 16:09:14 -0500

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    version='0.0.1',
+    version='0.0.2',
     name='conflagration',
     author='dwalleck and jidar',
     author_email='core.machinarum@gmail.com',
