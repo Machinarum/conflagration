0521508f6607b1c6cf4d4ee81a8bf19702b4abe0
Jose Idar <jose.idar@gmail.com>
2017-06-22 16:03:12 -0500

    Added tox and travisCI configuration. Fixed various pep8 and test errors. Added six dependency for py3.X compatability

--- tests/test_api.py
+++ tests/test_api.py
@@ -18,42 +18,42 @@ class Test_parse_dirs(Fixture):
     def test_parse_dirs_returns_empty_list_on_empty_dir_list(self):
         r = api._parse_dirs([])
         self.assertIsInstance(r, list)
-        self.assertEquals(r, list())
+        self.assertEqual(r, list())
 
     def test_dotstring_to_nested_dict_dotted_key(self):
         test_dict = {}
         expected = {'key':{'subkey':'value'}}
         r = api._dotstring_to_nested_dict(
             test_dict, ['key', 'subkey'], 'value')
-        self.assertEquals(expected, r)
+        self.assertEqual(expected, r)
 
     def test_dotstring_to_nested_dict_flat_key(self):
         test_dict = {}
         expected = {'key':'value'}
         r = api._dotstring_to_nested_dict(
             test_dict, ['key'], 'value')
-        self.assertEquals(expected, r)
+        self.assertEqual(expected, r)
 
     def test_dotstring_to_nested_dict_dotted_existing_key(self):
         test_dict = {'key': {'subkey':'value0'}}
         expected = {'key':{'subkey':'value1'}}
         r = api._dotstring_to_nested_dict(
             test_dict, ['key', 'subkey'], 'value1')
-        self.assertEquals(expected, r)
+        self.assertEqual(expected, r)
 
     def test_dotstring_to_nested_dict_flat_existing_key(self):
         test_dict = {'key': 'value0'}
         expected = {'key':'value1'}
         r = api._dotstring_to_nested_dict(
             test_dict, ['key'], 'value1')
-        self.assertEquals(expected, r)
+        self.assertEqual(expected, r)
 
     @mock.patch("conflagration.api.namedtuple")
     def test_dict_to_nt_flat_dict(self, mnt):
         mnt.return_value = dict
         data = {'key': 'value'}
         r = api._dict_to_nt(data, 'name')
-        self.assertEquals(data, r)
+        self.assertEqual(data, r)
 
 
     @mock.patch("conflagration.api.namedtuple")
@@ -61,7 +61,7 @@ class Test_parse_dirs(Fixture):
         mnt.return_value = dict
         data = {'key': {'subkey':'value'}}
         r = api._dict_to_nt(data, 'name')
-        self.assertEquals(data, r)
+        self.assertEqual(data, r)
 
     @mock.patch("conflagration.api._dict_to_nt")
     @mock.patch("conflagration.api._dotstring_to_nested_dict")
@@ -70,7 +70,7 @@ class Test_parse_dirs(Fixture):
         mdtnd.return_value = 'VALUE1'
         mdtn.return_value = 'VALUE2'
         r = api._build_super_namedtuple(inputd, 'name')
-        self.assertEquals(r, 'VALUE2')
+        self.assertEqual(r, 'VALUE2')
         mdtnd.assert_called_once_with(dict(), ['key', 'subkey'], 'VALUE0')
         mdtn.assert_called_once_with(dict(), 'name')
 
@@ -86,5 +86,5 @@ class Test_parse_dirs(Fixture):
              os.path.join('dirpath1', 'file2')])
 
         files = api._parse_dirs(['dirpath1', 'dirpath2'])
-        self.assertEquals(mockwalk.call_count, 2)
-        self.assertEquals(expected, files)
+        self.assertEqual(mockwalk.call_count, 2)
+        self.assertEqual(expected, files)
