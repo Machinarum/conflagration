b12aa6c5656a959c9082fc8734bac2df501aa72e
Jose Idar <jose.idar@gmail.com>
2017-07-06 14:12:55 -0500

    Environment variable keys  can now be separated by any user defined string, and default to a double underscore '__'

--- conflagration/wrap.py
+++ conflagration/wrap.py
@@ -19,18 +19,24 @@ def config_file(file, raise_conflicts=False):
     return ret
 
 
-def environment(prefix='env.'):
+def environment(prefix, separator):
+    """Returns a dictionary of all relavent environment variables and their
+    values, with the prefix stripped from the keys."""
     env_vars = os.environ
     filtered_vars = {
         k: v for k, v in env_vars.iteritems() if k.startswith(prefix)
     }
     fdict = {}
     for k, v in filtered_vars.iteritems():
-        _keysplit = k.split(prefix)
+        _keysplit = k.split("{}{}".format(prefix, separator))
         new_key = None
         if len(_keysplit) > 1:
             new_key = _keysplit[1]
         elif len(_keysplit) == 1:
             new_key = _keysplit[0]
+        # The rest of the codebase expects the separator to be a dot, but env
+        # vars can't have dots in the name.
+        new_key = new_key.replace(separator, ".")
         fdict[new_key] = filtered_vars[k]
+
     return fdict
