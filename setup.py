5004bc694b5594340de8292a89e0133ec656a536
Jose Idar <jose.idar@gmail.com>
2017-07-12 16:35:05 -0500

    Bump version: 0.0.6 → 0.0.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    version='0.0.6',
+    version='0.0.7',
     name='conflagration',
     author='dwalleck and jidar',
     author_email='core.machinarum@gmail.com',
