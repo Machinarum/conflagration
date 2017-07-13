cb10c15d46529275ad108e27ebceace1fd4c3036
Jose Idar <jose.idar@rackspace.com>
2017-07-13 15:49:16 -0500

    Added ModifiableNamespace factory object, and flags for controlling custom and default namespace behavior

--- conflagration/api.py
+++ conflagration/api.py
@@ -75,7 +75,8 @@ Examples:
 
 def conflagration(
         files=None, dirs=None, allow_env_override=True, default_to_env=False,
-        raise_conflicts=True, env_var_prefix='env', env_var_separator='__'):
+        raise_conflicts=True, env_var_prefix='env', env_var_separator='__',
+        lowercase_keys=False, namespace_obj=None):
     """
     :param files: A list of paths to config files.
     :param dirs: A list of directories that is presumed to contain only config
@@ -88,7 +89,21 @@ def conflagration(
         sources to override the environment variables.
     :param raise_conflicts: If True, raises an exception if conflicting values
         exist for the same conflagration address key among all config files.
+    :param env_var_prefix: conflagration will expect env vars to start with
+        this string
+    :param env_var_separator: conflagration will expect env var keys to follow
+        the "prefix<sep>section<sep>key" pattern where <sep> is this string.
+    :param lowercase_keys: If True, conflagration will automatically lowercase
+        all keys in the final namespace object.
+    :param namespace_obj:You can pass in a custom ModifiableNamespace object
+        as created via the ModifiableNamespace class in conflagration.namespace
+        By default, the namespace object will lowercase all keys.  If you pass
+        one in, it will override the lowercase_keys flag.
     """
+
+    #TODO: Make the interaction between the default ns behavior and a custom ns
+    #      less clunky
+
     files = files or list()
     dirs = dirs or list()
     dir_files = _parse_dirs(dirs)
@@ -108,7 +123,14 @@ def conflagration(
     if allow_env_override:
         _filedict.update(_envdict)
 
-    return _build_namespace(address_dict=_filedict)
+    if not namespace_obj:
+        mods = [namespace.modifiers.KeyMapper({'self': 'self_'})]
+        if lowercase_keys:
+            modifier_list.insert(0, namespace.modifiers.LowerCaseKeys())
+        namespace_obj = namespace.ModifiableNamespace(modifier_list=mods)
+
+    return _build_namespace(
+        address_dict=_filedict, namespace_obj=namespace_obj)
 
 
 def _dotstring_to_nested_dict(return_dict, splitkey_list, value):
@@ -148,8 +170,9 @@ def _dotstring_to_nested_dict(return_dict, splitkey_list, value):
     return return_dict
 
 
-def _build_namespace(address_dict, separator='.'):
+def _build_namespace(address_dict, separator='.', namespace_obj=None):
     nested_address_dict = dict()
+    namespace_obj = namespace_obj or namespace.ModifiableNamespace()
     for k, v in address_dict.items():
         _dotstring_to_nested_dict(
             return_dict=nested_address_dict,
@@ -157,7 +180,7 @@ def _build_namespace(address_dict, separator='.'):
             value=v)
 
     jdict = json.dumps(nested_address_dict)
-    return json.loads(jdict, object_hook=namespace.namespace_hook)
+    return json.loads(jdict, object_hook=namespace_obj)
 
 
 def _parse_dirs(dirs):
