7e7b35388c9e9626480c79346b39879a2339f5aa
Daryl Walleck <dwalleck@gmail.com>
2018-04-12 16:44:10 -0500

    Bump version: 0.0.9 → 0.0.10

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    version='0.0.9',
+    version='0.0.10',
     name='conflagration',
     author='dwalleck and jidar',
     author_email='core.machinarum@gmail.com',
